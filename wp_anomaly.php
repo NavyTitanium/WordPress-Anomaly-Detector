c48a97aee9b7bb5f0c6362c3578dfc1c0ff53908
NavyTitanium <33707347+NavyTitanium@users.noreply.github.com>
2021-10-19 16:41:04 -0400

    Create wp_anomaly.php

---
+++ wp_anomaly.php
@@ -0,0 +1,116 @@
+import sys
+import os
+import requests
+import urllib3
+from bs4 import BeautifulSoup
+from termcolor import colored
+
+
+wp_basepath=os.path.join(os.path.dirname(os.path.abspath(__file__)),"WordPress-5.8.1")
+crawlable_dir= {}
+UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def print_results(folders,files):
+    for folder in sorted(folders):
+        if "/plugins/" in folder:
+            print(colored("[Plugin]",'green'),colored(folder,'cyan'))
+        elif "/themes/" in folder:
+            print(colored("[Theme]",'green'),colored(folder,'magenta'))
+        else:
+            print(colored("[Directory]",'yellow'),colored(folder,'blue'))
+
+    for file in sorted(files):
+        print(colored("[File]",'yellow'),colored(file,'red'))
+
+def find_anomalies(folders,files,path,level):
+    anomalies_files=[]
+    anomalies_folders=[]
+    for file in files:
+        if file not in crawlable_dir[path]:
+            anomalies_files.append(path +"/"+file)
+    for folder in folders:
+        if path+"/"+folder.strip()[:-1] not in crawlable_dir.keys():
+            anomalies_folders.append(path +"/"+folder)
+
+    if len(anomalies_folders)>0 or len(anomalies_files)>0:
+        print_results(anomalies_folders,anomalies_files)
+
+def parse_page(content):
+    folders=[]
+    files=[]
+    soup = BeautifulSoup(content, 'html.parser')
+    for link in soup.find_all('a'):
+        file_or_folder=link.get('href')
+        if "parent directory" not in link.get_text().lower():
+            if "?" not in file_or_folder and ";" not in file_or_folder:
+                if "/" in file_or_folder:
+                    folders.append(file_or_folder)
+                elif ".php" in file_or_folder:
+                    files.append(file_or_folder)
+
+    return folders,files
+def is_directoy_listing_enable(url):
+    try:
+        resp=requests.get(url, verify=False,timeout=5, headers={'User-Agent': UA})
+        if '<h1>index of' in resp.text.lower() or 'href="?C=D;O=A">' in resp.text.lower():
+            return True,resp.text
+        return False,""
+    except Exception as e:
+        print("ERROR: "+str(e))
+        return False,""
+
+
+def subdirectory_depth(level):
+    keys=[]
+    for key in crawlable_dir:
+        if key.count(os.path.sep)==level:
+            keys.append(key)
+    return keys
+
+def load_files():
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(wp_basepath):
+        directory=os.path.split(r)[0]
+        if directory not in crawlable_dir: crawlable_dir[directory]=[]
+        for file in f:
+            path=(os.path.join(r, file).split(wp_basepath)[1])
+            directory=os.path.split(path)[0]
+            if directory not in crawlable_dir: crawlable_dir[directory]=[]
+            if ".php" in file and directory != "/":
+                crawlable_dir[directory].append(os.path.split(path)[1])
+
+    to_append=[]
+    for key in crawlable_dir:
+        if "/" in key:
+            subfolder=os.path.split(key)[0]
+            if subfolder not in crawlable_dir and subfolder !="/" and subfolder not in to_append:
+                to_append.append(subfolder)
+    for items in to_append:
+        crawlable_dir[items]=[]
+    return crawlable_dir
+def main(website):
+    crawlable_dir=load_files()
+    nb_items= sum([len(x) for x in crawlable_dir.values()])
+    print("[*] "+str(len(crawlable_dir)) + " subdirectories and " +str(nb_items) + " PHP files loaded for comparison")
+
+    for level in range(4):
+        level_alerted=False
+        for path in subdirectory_depth(level):
+            folders=[]
+            files=[]
+            isEnable,response=is_directoy_listing_enable(website+path)
+            if isEnable:
+                if not level_alerted:
+                    print("[*] Results for directories depth level "+str(level))
+                    level_alerted=True
+                folders,files=parse_page(response)
+                find_anomalies(folders,files,path,level)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('Usage: ' + os.path.basename(__file__) + ' http://example.com')
+        print('The argument should be the URL of the WordPress site to scan')
+        exit(1)
+
+    main(sys.argv[1])
